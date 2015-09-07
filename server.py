ba01b45e56e45b0227e70f886b57bd7e8d1da72d
Thomas Parks <thomasparks@outlook.com>
2015-09-07 14:56:00 +0100

    Moved remoteproc stuff into a seperate class.

--- server.py
+++ server.py
@@ -4,8 +4,6 @@ import Pyro4
 import subprocess
 import time
 import os
-import uuid
-from PyRedPitaya.board import RedPitaya
 
 COMM_RX_AT_ROWS = 0xFFFF9010
 COMM_RX_AT_FLAG = 0xFFFF9014
@@ -16,12 +14,60 @@ def bin(s):
     ''' Returns the set bits in a positive int as a str.'''
     return str(s) if s<=1 else bin(s>>1) + str(s&1)
 
+
+class app_cpu1(object):
+
+    def __init__(self):
+        self.OCM = open('/dev/mem', 'r+')
+        self.up = open('/sys/devices/soc0/1e000000.remoteproc/remoteproc0/up', 'w+')
+        self.remoteProcLoaded = False
+        self.running = False
+
+    def load(self, times, digitals, analogA, analogB):
+        # unsigned long - no rows
+        struct.pack_into('L', self.OCM, COMM_RX_AT_ROWS, len(times)) # write the no of actiontable rows
+        self.OCM.flush()
+        for time, DigitalBitMask, analogA, analogB in zip(times, digitals,
+                                                          analogA, analogB):
+            nanos = time * 1e9
+            while struct.unpack('i', self.OCM, COMM_RX_AT_FLAG) != 0:
+                pass # wait for the value to be consumed
+            # nanos, clocks, pins, a1, a2
+            struct.pack_into('QQLLL', self.OCM, COMM_RX_AT, time,
+                                                            DigitalBitMask,
+                                                            analogA,
+                                                            analogB)
+            struct.pack_into('i', self.OCM, COMM_RX_AT_FLAG, 1)
+
+    def _load_module(self):
+        if !self.remoteProcLoaded:
+            subprocess.Popen('modprobe zynq_remoteproc')
+            self.remoteProcLoaded = True
+
+    def _unload_module(self):
+        subprocess.Popen('rmmod -f zynq_remoteproc')
+        self.remoteProcLoaded = False
+
+    def abort(self):
+        self.up.write(0)
+        self.up.flush()
+        self.running = True
+        self._unload_module()
+
+    def start(self):
+        self._load_module()
+        self.up.write(1)
+        self.up.flush()
+        self.running = True
+
+    def stop(self):
+        self.abort()
+
+
+
 class rpServer(object):
 
     def __init__(self):
-        self.timeLineValues = []
-        self.profileFile = None
-        self.red_pitaya = RedPitaya()
         self.pid = None
         self.name = None
         self.times = []
@@ -29,7 +75,9 @@ class rpServer(object):
         self.analogA = []
         self.analogB = []
 
-        self.OCM = open('/dev/mem', 'r+')
+        self.appcpu1 = app_cpu1()
+
+
 
     ## In order to just toggle a pin, we create a short actionTable
     # turning the pin on and then off after 5 secs.
@@ -60,7 +108,7 @@ class rpServer(object):
     # The dsp has a handler for SIGINT that cleans up
     def Abort(self):
         # kill the server process
-        subprocess.call(['kill', '-9', str(self.pid)])
+        self.appcpu1.abort()
 
     def MoveAbsoluteADU(self, aline, aduPos):
         # probably just use the python lib
@@ -81,31 +129,13 @@ class rpServer(object):
         self.analogB = analogs[1]
 
     def DownloadProfile(self): # This is saving the action table
-        # unsigned long - no rows
-        struct.pack_into('L', self.OCM, COMM_RX_AT_ROWS, len(times)) # write the no of actiontable rows
-        self.OCM.flush()
-        for time, DigitalBitMask, analogA, analogB in zip(self.times, self.digitals,
-                                                          self.analogA, self.analogB):
-            nanos = time * 1e9
-            while struct.unpack('i', self.OCM, COMM_RX_AT_FLAG) != 0:
-                pass # wait for the value to be consumed
-            # nanos, clocks, pins, a1, a2
-            struct.pack_into('QQLLL', self.OCM, COMM_RX_AT, time,
-                                                            DigitalBitMask,
-                                                            analogA,
-                                                            analogB)
-            struct.pack_into('i', self.OCM, COMM_RX_AT_FLAG, 1)
-
+        self.appcpu1.load(self.times, self.digitals, self.analogA, self.analogB)
 
     def InitProfile(self, numReps):
-        self.name = 'profile'
-        self.timeLineValues = []
-        self.profileFile = open(self.name, 'w')
+        self.times, self.digitals, self.analogA, self.analogB = [], [], [], []
 
     def trigCollect(self):
-        cmd = ['./dsp', os.path.join(os.getcwd(), self.name)]
-        print('calling', cmd)
-        self.pid = subprocess.Popen(cmd).pid
+        self.appcpu1.start()
 
     def ReadPosition(self):
         pass
@@ -113,13 +143,13 @@ class rpServer(object):
     def WriteDigital(self, level):
         self.red_pitaya.hk.led = level
 
+if __name__ == '__main__':
+    dsp = rpServer()
 
-dsp = rpServer()
+    print("providing dsp.d() as [pyroDSP] on port 7766")
+    print("Started program at",time.strftime("%A, %B %d, %I:%M %p"))
 
-print("providing dsp.d() as [pyroDSP] on port 7766")
-print("Started program at",time.strftime("%A, %B %d, %I:%M %p"))
-
-import random
-daemon = Pyro4.Daemon(port = random.randint(2000, 10000), host = '192.168.1.100')
-Pyro4.Daemon.serveSimple({dsp: 'pyroDSP'},
-        daemon = daemon, ns = False, verbose = True)
+    import random
+    daemon = Pyro4.Daemon(port = random.randint(2000, 10000), host = '192.168.1.100')
+    Pyro4.Daemon.serveSimple({dsp: 'pyroDSP'},
+            daemon = daemon, ns = False, verbose = True)
